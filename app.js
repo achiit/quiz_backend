745c8dd90c2109a11db3707a71c56c4dcaeec637
achiit <as4656@srmist.edu.in>
2023-09-25 02:00:44 +0530

    added for normal

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ app.use(bodyParser.json());
 // Routes
 app.use('/admin', adminRoutes);
 app.use('/user', userRoutes);
-
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the quiz app API' });
+});
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
